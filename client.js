266dadf59c29ac175996bfc7f0ff12d476aab30d
Christopher <christopherstoy@gmail.com>
2020-08-26 20:48:02 +0000

    added different methods of moving

--- client.js
+++ client.js
@@ -11,7 +11,31 @@ const connect = function() {
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
     conn.write("Name: TOY");
+
+  // Set Timeout Test 
+  // const moves = ["Move: up", "Move: left", "Move: up", "Move: up", "Move: right"];
+  //   let delay = 0
+  //   for (const move of moves) {
+  //     setTimeout (() => {
+  //       conn.write(move)
+  //   }, delay);
+  //   delay += 1000;
+  // }  
+
+
+// Interval Move Test
+// const move = function(moves) {
+//     for (const move of moves){
+//       conn.write(move)
+//     }
+//   }
+
+//   setInterval(() => {
+//     move(moves)
+//   }, 1000);
+
   })
+
 
   conn.on('data', (data) => {
     console.log(data);
