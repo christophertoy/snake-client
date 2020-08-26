2093bd5b218a2de98abf485d1c03f5a86a3e73fd
Christopher <christopherstoy@gmail.com>
2020-08-26 22:56:40 +0000

    updated arrow movements

--- input.js
+++ input.js
@@ -1,10 +1,24 @@
-const setupInput = function() {
+let connection;
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
 
   stdin.on('data', (handleUserInput))
+
+  stdin.on('data', (data) => {
+    if (data === "w") {
+      conn.write("Move: up")
+    } else if (data === "a") {
+      conn.write("Move: left")
+    } else if (data === "s") {
+      conn.write("Move: down")
+    } else if (data === 'd') {
+      conn.write("Move: right");
+    }
+  })
   return stdin;
   }
 
@@ -13,6 +27,7 @@ const setupInput = function() {
       process.exit();
     };
   }
+
 
   module.exports = {
     setupInput
